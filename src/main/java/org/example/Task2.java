3f2e668c68a3bc6e54c31a6a7c78cfbd729ec91e
DenWays <130283554+DenWays@users.noreply.github.com>
2024-09-24 10:03:28 +0500

    Complete Task2

--- src/main/java/org/example/Task2.java
+++ src/main/java/org/example/Task2.java
@@ -1,4 +1,24 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Task2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        String[] partsInput = input.split("[.]");
+
+        if(partsInput.length != 2) {
+            System.out.println("Введенная строка не является десятичным числом");
+            return;
+        }
+
+        System.out.println("Введенная строка является десятичным числом");
+
+        if(partsInput[0].charAt(0) == '+' || partsInput[0].charAt(0) == '-') {
+            partsInput[0] = partsInput[0].substring(1);
+        }
+        System.out.println("Количество знаков в целой части: " + partsInput[0].length());
+        System.out.println("Количество знаков в дробной части: " + partsInput[1].length());
+    }
 }
