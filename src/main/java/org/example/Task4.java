b320086f1b2079e245d1fb4d0b7a5cfb1fcd923d
DenWays <130283554+DenWays@users.noreply.github.com>
2024-09-24 10:22:36 +0500

    Complete Task4

--- src/main/java/org/example/Task4.java
+++ src/main/java/org/example/Task4.java
@@ -1,5 +1,31 @@
 package org.example;
 
-public class Task4 {
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Scanner;
 
+public class Task4 {
+    public static void main(String[] args) throws FileNotFoundException {
+        FileReader fileReader = new FileReader("input2.txt");
+        Scanner scanner = new Scanner(fileReader);
+        HashSet<String> set = new HashSet<>();
+
+        ArrayList<String> lines = new ArrayList<>();
+        while (scanner.hasNextLine()) {
+            lines.add(scanner.nextLine().toLowerCase());
+        }
+
+        for (String line : lines) {
+            String[] words = line.split(" ");
+            for (String word : words) {
+                set.add(word);
+            }
+        }
+
+        for (String word : set) {
+            System.out.print(word + " ");
+        }
+    }
 }
