2306eb575fda45bbc990a06e400f9d38e2dd6036
DenWays <130283554+DenWays@users.noreply.github.com>
2024-09-24 09:53:26 +0500

    Complete Task1

--- src/main/java/org/example/Task1.java
+++ src/main/java/org/example/Task1.java
@@ -1,7 +1,23 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        String[] words = input.split(" ");
 
+        for (int i = 0; i < words.length; i++) {
+            words[i] = words[i].replace("A", "a"); // английйские символы "A" и "a"
+        }
+
+        int countWordWithA = 0;
+        for (String word : words) {
+            if (word.charAt(0) == 'a') {
+                countWordWithA++;
+            }
+        }
+        System.out.println(countWordWithA);
     }
 }
